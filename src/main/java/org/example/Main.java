c1998f5a925148d8262ce64005826c050d21ea9d
Rupert Pfiser <rupert.pfister@gmail.com>
2023-07-18 18:06:57 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a; // Variablen Deklaration
+        a = 42; // Variablen Initialisierung
+        int b = 42;
+    }
+}
